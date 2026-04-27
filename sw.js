61ac1ae223de3209d9444f44470241075eb08dc2
asdfmake <makevic2003@gmail.com>
2026-04-27 15:55:03 +0200

    Updated serviceworking with less agressive caching strategy

--- sw.js
+++ sw.js
@@ -1,38 +1,14 @@
-const CACHE_NAME = 'geekbit-v2';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/main.js',
-  '/manifest.json',
-  '/assets/img/neon-logo1.png'
-];
-
-// Instalacija Service Workera
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-// Aktivacija
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Fetch (Hvatanje zahteva)
 self.addEventListener('fetch', (event) => {
+  const request = event.request;
+
+  if (request.url.includes('.js') || request.url.includes('.css')) {
+    event.respondWith(fetch(request));
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
+    caches.match(request).then((cached) => {
+      return cached || fetch(request);
     })
   );
 });
